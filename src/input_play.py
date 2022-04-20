76566b2163351cbe57b27e1b549082ec50b9f94f
root <root@localhost>
2022-04-20 22:52:01 +0600

    auto publish 1650473521050

--- src/input_play.py
+++ src/input_play.py
@@ -1,1 +1,26 @@
+#declare an jnput
+input("this is a text input")
+
+# input can store in a variable
+user_name = input("Enter your name here :")
+user_age = input("Enter your age here :")
+user_dob = input("Enter your birth date here")
+user_pass = input("Enter password here :")
+
+# age should be an integer num
+# i have to convert the age string to int num
+int_age = int(user_age)
+
+print(int_age)
+
+
+
+
+
+
+
+
+
+
+
 
