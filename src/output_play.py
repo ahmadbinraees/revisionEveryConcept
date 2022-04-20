9b2a567d36bdb2bf0ca06527108b28a03b0fb515
root <root@localhost>
2022-04-20 15:30:00 +0600

    auto publish 1650447000381

--- src/output_play.py
+++ src/output_play.py
@@ -1,1 +1,21 @@
+#output keyword
+print("This is an output keyword")
+
+#number output
+print("This is a numeric output =",23)
+
+#math output
+print("2 + 2 =",2 + 2)
+
+print("8 - 4 =",8 - 4)
+
+print("2 x 2 =",2 * 2)
+
+print("8 / 2 =",8 / 2)
+
+
+
+
+
+
 
--- src/variable_play.py
+++ src/variable_play.py
@@ -1,1 +1,73 @@
 
+# declaring variable
+# a variable should have a name , equal sign , value
+example = "This is a variable"
+
+nd_example = 2
+
+#print(example, nd_example)
+
+# there is 3 types of variable
+# numeric , string , boolean
+
+# this is a numeric variable
+your_age = 12
+
+# this is a string type variable
+your_name = "Tom"
+
+# those are boolean
+you_are_adult = False
+you_are_boy = True
+
+
+# lets play with variables 
+# i will operation variable with another variable
+
+character_cat = "Tom"
+character_rat = "Jerry"
+
+show_name = character_cat +" & "+ character_rat
+
+#print(show_name)
+
+
+num_two   = 2
+num_four  = 4
+num_six   = 6
+num_eight = 8
+num_ten   = 10
+num_sixtin= 16
+
+plus    = num_two    + num_six
+minus   = num_ten    - num_two 
+multiply= num_four   * num_two 
+devide  = num_sixtin / num_two
+
+print("2 + 6 =",plus)
+print("10 - 2 =",minus)
+print("4 * 2 =",multiply)
+print("16 / 2 =",devide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
