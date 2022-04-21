348015c84d45188c5aeb9669536a4e0e825e631f
root <root@localhost>
2022-04-21 12:46:40 +0600

    auto publish 1650523599777

--- src/conditions_play.py
+++ src/conditions_play.py
@@ -1,1 +1,37 @@
 
+m_price = 500
+c_price = 100
+s_price = 600
+
+u_rich = True
+u_rich = False
+
+u_budget= 400
+u_budget= 1000
+
+if u_budget > m_price or u_rich == True:
+  print("I will buy Iphone max pro")
+
+else :
+  print("I will Happy with Calculator")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
